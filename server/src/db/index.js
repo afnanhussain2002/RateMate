857a276f1bb468b00d20f752c41f4b4ced6fc4dd
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-25 09:01:53 +0600

    write full func for connect mongoDB and export it

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,10 +1,15 @@
+import mongoose from "mongoose"
 import dotenv from "dotenv"
+import { DB_NAME } from "../constants";
 
 const connectDB = async() =>{
     try {
-        
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDB connection error", error);
         process.exit(1)
     }
 }
+
+export default connectDB
