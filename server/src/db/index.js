9b640045fb45d653b755885124d7d512980c2d59
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-25 09:29:25 +0600

    connected mongoDB

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose"
-import dotenv from "dotenv"
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async() =>{
     try {
---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv"
+import { app } from "./app.js";
+import { configDotenv } from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config()
+
+
+
+const port = process.env.PORT || 8000;
+connectDB()
+.then(() =>{
+    app.on("error", error =>{
+        console.log('error from express', error);
+    })
+    app.listen(port, () =>{
+        console.log(`Server is running at port: ${port}`);
+    })
+})
+.catch(error =>{
+    console.log('mongoDB connection failed!!!', error);
+  })
+
