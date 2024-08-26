e454ddde0eb826cfc2f39f008782f8c99b9cdb12
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-26 06:52:04 +0600

    make full model for client text reviews

--- server/src/models/clientReview.text.model.js
+++ server/src/models/clientReview.text.model.js
@@ -1,6 +1,37 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema, SchemaType } from "mongoose";
 
 const clientTextReviewSchema = new Schema({
+
+    fullName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        unique: true,
+        lowercase:true,
+        trim:true,
+        required: true
+    },
+    avatar:{
+        type:String,
+        required: true
+    },
+    rating:{
+        type:Number,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    avatar:{
+        type:String,
+    },
+    reviewFor:{
+        type: Schema.Types.ObjectId,
+        ref:"User"
+    }
 
 }, {timestamps:true})
 
