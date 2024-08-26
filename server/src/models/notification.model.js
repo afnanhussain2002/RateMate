74f784cd3f956088c461140d55879eb059c1f836
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-26 07:44:30 +0600

    make a file for notification schema

---
+++ server/src/models/notification.model.js
@@ -0,0 +1,5 @@
+import mongoose, { Schema } from "mongoose";
+
+const notificationSchema = new Schema({},{timestamps:true})
+
+export const Notification = mongoose.model("Notification", notificationSchema)
