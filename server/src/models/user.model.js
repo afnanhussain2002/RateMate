d3321d68d8a4053e59cd4f88db9fc99bfe12d92f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-26 08:43:43 +0600

    write method for generate access token from jwt

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -46,5 +46,17 @@ userSchema.methods.isCorrectPassword = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        {
+            _id:this._id
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+           expiresIn: process.env.ACCESS_TOKEN_EXPIRED
+        }
+    )
+}
+
 
 export const User = mongoose.model("User", userSchema)
