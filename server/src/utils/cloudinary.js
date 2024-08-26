f41218ccdebe236ae12aa5a6e2011285b9c7a75b
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-26 10:16:03 +0600

    handle the cloudinary file upload success

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -7,3 +7,17 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+const uploadOnCloudinary = async(localFilePath) =>{
+    try {
+        if (!localFilePath) return null
+
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        console.log("File is upload on cloudinary", response);
+        fs.unlink(localFilePath)
+    } catch (error) {
+        
+    }
+}
