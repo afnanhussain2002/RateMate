684d342e129f6e7bef86bcede837c6716a650eb5
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-26 07:41:35 +0600

    make full schema for store review bundles

--- server/src/models/reviewBundle.model.js
+++ server/src/models/reviewBundle.model.js
@@ -1,5 +1,31 @@
-import { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const reviewBundleSchema = new Schema({
+     name:{
+        type:String,
+        required:true,
+     },
+     description:{
+        type:String,
+        required:true
+     },
+     textReviews:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"ClientText"
+        }
+     ],
+     videoReviews:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"ClientVideo"
+        }
+     ],
+     owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+     }
 
 },{timestamps:true})
+
+export const ReviewBundle = mongoose.model("ReviewBundle", reviewBundleSchema)
