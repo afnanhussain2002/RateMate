605572c9a35e3d1368b080f0fbdcab83ca52f066
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-26 07:46:54 +0600

    make full schema for notificatins

--- server/src/models/notification.model.js
+++ server/src/models/notification.model.js
@@ -1,5 +1,17 @@
 import mongoose, { Schema } from "mongoose";
 
-const notificationSchema = new Schema({},{timestamps:true})
+const notificationSchema = new Schema({
+    message:{
+        type:String,
+    },
+    isRead:{
+        type:Boolean,
+        default:false
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
 
 export const Notification = mongoose.model("Notification", notificationSchema)
