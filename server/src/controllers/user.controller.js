3d5358ea07ff750a3e8bb7fb185353d910f9cf86
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-28 17:53:39 +0600

    make file for user router

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,7 +1,27 @@
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/ApiError";
 import asyncHandler from "../utils/asyncHandler";
 
 
 const registerUser = asyncHandler(async(req,res) =>{
+ const {fullName, email, username, password} = req.body;
+
+ if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existUser = await User.findOne({
+    $or:[{username}, {email}]
+  })
+
+  if (existUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  const avatarLocalPath 
+
 
 })
 
