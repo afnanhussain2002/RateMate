e7e61f72a61addab500da5df364674b4996219d0
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-29 07:03:33 +0600

    make a route for login the user

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 const userRouter = Router();
 
 userRouter.route("/register").post(upload.single("avatar"), registerUser);
+userRouter.route("/login").post(loginUser)
 
 export default userRouter;
