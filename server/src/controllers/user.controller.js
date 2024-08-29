c991a64388100f9600f9e53609e15fa4a6c7f858
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-29 07:10:04 +0600

    add a validation on login user

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -85,6 +85,10 @@ const registerUser = asyncHandler(async(req, res) => {
 const loginUser = asyncHandler(async(req,res) =>{
   const {username, email, password} = req.body
 
+  if (!(username || email)) {
+    throw new ApiError(400, "username or email is required");
+  }
+ 
 
 })
 
