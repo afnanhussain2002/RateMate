9b62f0a79efe7fbd24b2051730fe4dcc3649c883
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-01 21:49:52 +0600

    handle catch part of that func

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,5 +1,10 @@
+import { ApiError } from "../utils/ApiError";
 import asyncHandler from "../utils/asyncHandler";
 
 export const verifyJWT = asyncHandler(async(req,res) =>{
-    
+    try {
+        
+    } catch (error) {
+       throw new ApiError(401, error?.message || "Invalid access token") 
+    }
 })
