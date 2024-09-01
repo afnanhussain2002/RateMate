72ef36acae65bf63562b93ca14c765573620dee8
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-01 23:36:49 +0600

    write route for access token refreshed

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const userRouter = Router();
 
 userRouter.route("/register").post(upload.single("avatar"), registerUser);
 userRouter.route("/login").post(loginUser)
 userRouter.route("/logout").post(verifyJWT,logoutUser)
+userRouter.route("refresh-token").post(refreshAccessToken)
 
 export default userRouter;
