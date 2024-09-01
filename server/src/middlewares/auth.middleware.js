d90612c72ecefd1abf2d3009030271d67b562d7f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-01 21:52:39 +0600

    get the token from cookie

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ import asyncHandler from "../utils/asyncHandler";
 
 export const verifyJWT = asyncHandler(async(req,res) =>{
     try {
-        
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
     } catch (error) {
        throw new ApiError(401, error?.message || "Invalid access token") 
     }
