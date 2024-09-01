16d94e7931fa43d0c90d4724fc0f30f13f7c649e
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-01 21:55:33 +0600

    import jwt

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,5 +1,6 @@
 import { ApiError } from "../utils/ApiError";
 import asyncHandler from "../utils/asyncHandler";
+import jwt from "jsonwebtoken"
 
 export const verifyJWT = asyncHandler(async(req,res) =>{
     try {
@@ -8,6 +9,8 @@ export const verifyJWT = asyncHandler(async(req,res) =>{
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
         }
+
+        const decodeToken = jwt
     } catch (error) {
        throw new ApiError(401, error?.message || "Invalid access token") 
     }
