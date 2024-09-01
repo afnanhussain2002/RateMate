d3724610580ad304b291d3bf8d51c032c061a7ef
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-01 21:54:46 +0600

    write a validation for if not token found

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -4,6 +4,10 @@ import asyncHandler from "../utils/asyncHandler";
 export const verifyJWT = asyncHandler(async(req,res) =>{
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request")
+        }
     } catch (error) {
        throw new ApiError(401, error?.message || "Invalid access token") 
     }
