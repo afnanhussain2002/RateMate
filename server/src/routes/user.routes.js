4aced549d8c567f7c66a30574dec75d18a548db1
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-03 11:58:04 +0600

    add a route for get current user

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { changeCurrentPassword, loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const userRouter = Router();
 
@@ -9,5 +9,6 @@ userRouter.route("/login").post(loginUser)
 userRouter.route("/logout").post(verifyJWT,logoutUser)
 userRouter.route("refresh-token").post(refreshAccessToken)
 userRouter.route("/change-password").post(verifyJWT, changeCurrentPassword)
+userRouter.route("/current-user").get(verifyJWT, getCurrentUser)
 
 export default userRouter;
