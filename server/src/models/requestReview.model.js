57860ffada35ae8eec21c1f456198294f996c676
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-03 17:26:53 +0600

    write code for request for review

--- server/src/models/requestReview.model.js
+++ server/src/models/requestReview.model.js
@@ -3,6 +3,17 @@ import mongoose, { Schema } from "mongoose";
 
 const requestForReviewSchema = new Schema({
 
+    content:{
+        type:String,
+        required:true
+    },
+    sendBy:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+
+    
+
 },{timestamps:true})
 
 export const RequestForReview = mongoose.model("RequestForReview", requestForReviewSchema)
