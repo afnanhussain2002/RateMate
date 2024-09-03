d94e9bac170ce874c613afc9526c4c30f89d4402
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-03 12:33:13 +0600

    make a route for update avatar

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const userRouter = Router();
 
@@ -11,5 +11,6 @@ userRouter.route("refresh-token").post(refreshAccessToken)
 userRouter.route("/change-password").post(verifyJWT, changeCurrentPassword)
 userRouter.route("/current-user").get(verifyJWT, getCurrentUser)
 userRouter.route("/update-details").patch(verifyJWT, updateAccountDetails)
+userRouter.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 
 export default userRouter;
