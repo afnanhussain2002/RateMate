494c0eb9e2a7127bf98a1a886663260008289e22
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-03 23:06:10 +0600

    get content from user

--- server/src/controllers/requestReview.controller.js
+++ server/src/controllers/requestReview.controller.js
@@ -1,6 +1,13 @@
+import { ApiError } from "../utils/ApiError";
 import asyncHandler from "../utils/asyncHandler";
 
 const requestForReview = asyncHandler(async(req,res) =>{
+
+  const {content} =  req.body
+
+  if (!content) {
+    throw new ApiError(402, "Please write some content for sent request")
+  }
 
 })
 
