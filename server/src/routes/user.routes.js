d9ded8380ac3849f500745cb73845d2520634b25
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-03 12:09:13 +0600

    make a route for add that func

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const userRouter = Router();
 
@@ -10,5 +10,6 @@ userRouter.route("/logout").post(verifyJWT,logoutUser)
 userRouter.route("refresh-token").post(refreshAccessToken)
 userRouter.route("/change-password").post(verifyJWT, changeCurrentPassword)
 userRouter.route("/current-user").get(verifyJWT, getCurrentUser)
+userRouter.route("/update-details").patch(verifyJWT, updateAccountDetails)
 
 export default userRouter;
