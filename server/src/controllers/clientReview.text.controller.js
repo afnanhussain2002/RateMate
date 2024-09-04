b41029b49bf134633e8fe07c11018f087b942574
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-04 11:06:33 +0600

    get the id of request review

--- server/src/controllers/clientReview.text.controller.js
+++ server/src/controllers/clientReview.text.controller.js
@@ -2,12 +2,16 @@ import asyncHandler from "../utils/asyncHandler";
 
 
 const getTextReview = asyncHandler(async(req,res) =>{
+    // get the request review id
 //    get review from body
 // validate mandatory field
 // get the client avatar, and attach file, send them to the cloudinary and get the url
 // save the data on database and get the data
 
-const { fullName, email, companyName, rating, description, }= req.body
+const {requestId} = req.params
+
+const { fullName, email, companyName, rating, description }= req.body
+
 })
 
 export {getTextReview}
