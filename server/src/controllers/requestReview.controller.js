1724598355eca2b475c5f54495d59327f01c977a
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-04 10:15:02 +0600

    return a response for request review

--- server/src/controllers/requestReview.controller.js
+++ server/src/controllers/requestReview.controller.js
@@ -1,26 +1,29 @@
 import { RequestForReview } from "../models/requestReview.model";
 import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
 import asyncHandler from "../utils/asyncHandler";
 
-const requestForReview = asyncHandler(async(req,res) =>{
-
-  const {content} =  req.body
+const requestForReview = asyncHandler(async (req, res) => {
+  const { content } = req.body;
 
   if (!content) {
-    throw new ApiError(402, "Please write some content for sent request")
+    throw new ApiError(402, "Please write some content for sent request");
   }
 
   const sendRequest = await RequestForReview.create({
     content,
-    sendBy:req.user
-  })
+    sendBy: req.user,
+  });
 
-  const reviewRequest = await RequestForReview.findById(sendRequest._id)
+  const reviewRequest = await RequestForReview.findById(sendRequest._id);
 
   if (!reviewRequest) {
-    throw new ApiError(501, "Something went wrong when send request")
+    throw new ApiError(501, "Something went wrong when send request");
   }
 
-})
+  res
+    .status(202)
+    .json(new ApiResponse(202, reviewRequest, "request send successfully"));
+});
 
-export {requestForReview}
+export { requestForReview };
