4574513d4e1e9b9db7c2503f64793ea784249536
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-06 17:53:55 +0600

    fix some issues

--- server/src/routes/clientReview.video.routes.js
+++ server/src/routes/clientReview.video.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { getVideoReview } from "../controllers/clientReview.video.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
-const clientReviewVideoRouter = Router
+const clientReviewVideoRouter = Router()
 
 clientReviewVideoRouter.route("/video-review/:requestId").post(upload.fields([
     {
