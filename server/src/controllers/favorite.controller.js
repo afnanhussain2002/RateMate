8e71b13e7cd18b22739e43b69c0228bc72d0ceb9
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-13 11:38:48 +0600

    add a validation

--- server/src/controllers/favorite.controller.js
+++ server/src/controllers/favorite.controller.js
@@ -1,7 +1,12 @@
+import { ApiError } from "../utils/ApiError";
 import asyncHandler from "../utils/asyncHandler";
 
 const favoriteTextReviews = asyncHandler(async(req,res) =>{
   const textReviewId = req.params
+
+  if (!textReviewId) {
+    throw new ApiError(400, "Select the text review");
+  }
 
 
 })
